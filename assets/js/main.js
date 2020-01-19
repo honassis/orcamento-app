28b49f52c17db28935036de5c20e98909e026f05
Honassis Lopes <honassislopes@gmail.com>
2020-01-19 17:56:28 -0300

    logica de manipulacao de itens no carrinho

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var Json = [
         Id: 1,
         Title: "TesteJson",
         Image: "logo.png",
-        Value: "R$ 2,20",
+        Value: "2.42",
         Message: "MSG",
         Color: "red",
     },
@@ -11,7 +11,7 @@ var Json = [
         Id: 2,
         Title: "TesteJson",
         Image: "logo.png",
-        Value: "R$ 2,20",
+        Value: "6.12",
         Message: "MSG",
         Color: "red",
     },
@@ -19,7 +19,7 @@ var Json = [
         Id: 3,
         Title: "TesteJson",
         Image: "logo.png",
-        Value: "R$ 2,20",
+        Value: "5.52",
         Message: "MSG",
         Color: "red",
     },
@@ -27,15 +27,15 @@ var Json = [
         Id: 4,
         Title: "TesteJson",
         Image: "logo.png",
-        Value: "R$ 2,20",
+        Value: "2.42",
         Message: "MSGI",
         Color: "red",
     },
     {
         Id: 5,
-        Title: "TesteJson",
+        Title: "TesteJsonLLLL",
         Image: "logo.png",
-        Value: "R$ 2,20",
+        Value: "2.42",
         Message: "MSG",
         Color: "red",
     },
@@ -43,12 +43,14 @@ var Json = [
         Id: 6,
         Title: "TesteJson",
         Image: "logo.png",
-        Value: "R$ 2,20",
+        Value: "2.42",
         Message: "MSG",
         Color: "red",
     },
 
-]
+];
+//var to aux in search on json
+var AuxItemAdd = null;
 
 
 
@@ -60,22 +62,24 @@ function loadItem(Id,Title,Image,Value,Message){
                 </div>
                 <div class="produtos-content">
                     <div class="produto-info">
-                        <p class="produto-name">${Title}</p>
+                        <p class="produto-name">${Id}-${Title}</p>
                         <div class="info-prices">
                             <span class="produto-price">${Value}</span><span class="widget promotion">${Message}</span>
                         </div>
                     </div>
                     <div class="product-footer">
                         <input class="qtd" type="number" name="qtd" id="qtd_${Id}">
-                        <div class="btn btn-rmv" onclick="Add" >-1</div>
+                        <div class="btn btn-rmv" onclick="ProductAction('Subtract',${Id})" >-1</div>
                          
-                        <div class="btn btn-add" href="#">+1</div>
+                        <div class="btn btn-add" onclick="ProductAction('Add',${Id})">+1</div>
                     </div>
                 </div>
             </div> `;
             return product;
 }
-function loadSelectedItem(Id,Title,Image,Value,Message){
+function loadSelectedItem(Id,Title,Image,Value,Qnt){
+    var TotalP = Value*Qnt;
+    TotalP = TotalP.toFixed(2);
 var product = `
 <div class="car-item">
     <div class="car-img">
@@ -83,20 +87,20 @@ var product = `
     </div>
     <div class="car-item-content">
 
-        <p>${Title}</p>
+        <p>${Id} - ${Title}</p>
 
         <div class="car-item-footer">
             <div>
-                <button class="car-btn minus-btn">-1</button>
-                <button class="car-btn ">99</button>
-                <button class="car-btn more-btn">+1</button>
+                <button class="car-btn minus-btn" onclick="ProductAction('Subtract',${Id})">-1</button>
+                <button class="car-btn ">${Qnt}</button>
+                <button class="car-btn more-btn" onclick="ProductAction('Add',${Id})">+1</button>
             </div>
             <div class="car-item-money">
-                ${Value}
+                ${TotalP}
             </div>
         </div>
     </div>
-<i class="far fa-trash-alt car-item-remove"></i>
+<i class="far fa-trash-alt car-item-remove" onclick="ProductAction('Remove',${Id})"></i>
 </div>`;
  return product;
 }
@@ -139,10 +143,76 @@ function CarMarket(action){
         break;
     }
 }
-function je(){
-var htmll= loadSelectedItem(0,"TESTE JAVASCRIPT", "logo.png", "R$ 2.0","Testemsg");
-document.getElementById("car-container").innerHTML += htmll;
+function UpdateCart(){
+    document.getElementById("car-container").innerHTML = "";
+var ShoppingCart = localStorage.getItem("Shop");
+var ShoppingCart =  JSON.parse(ShoppingCart);
+var html = "";
+
+    for(var i =0; i < ShoppingCart.length;i++){
+        Product = FindPItem(ShoppingCart[i].Id);
+    
+     html+=loadSelectedItem(Product.Id, Product.Title, Product.Image, Product.Value,ShoppingCart[i].Qnt);
+    }
+    document.getElementById("car-container").innerHTML = html;
+
+
 }
-je();
-je();
-je();
+function AuxFind(Json) { 
+    return Json.Id === AuxItemAdd;
+}
+function FindPItem(Key){
+    AuxItemAdd = Key;
+    return Json.find(AuxFind);
+}
+var Test = [{
+    Id: 1,
+    Qnt: 12,
+},
+{
+    Id: 2,
+    Qnt:11,
+},
+{
+    Id: 3,
+    Qnt: 10,
+}];
+//localStorage.setItem("Shop", (JSON.stringify(Test)));
+console.log(localStorage.getItem("Shop"));
+UpdateCart();
+function ProductAction(Action, Id){
+    var ShoppingCart = localStorage.getItem("Shop");
+    var ShoppingCart =  JSON.parse(ShoppingCart);
+    Exists = false; // case no exists
+    for(var i=0;i < ShoppingCart.length;i++){
+        if(ShoppingCart[i].Id==Id){
+            switch(Action){
+                case "Add":
+                    ShoppingCart[i].Qnt++;
+                    Exists = true; 
+                break;
+                case "Subtract":
+                    ShoppingCart[i].Qnt--; 
+                    if(ShoppingCart[i].Qnt<=0){
+                        ShoppingCart.splice(i,1);
+                    }
+                break;
+                case "Remove":
+                    ShoppingCart.splice(i,1);
+                    break;
+            }
+        }
+
+    }
+    if(Action=="Add"&&Exists==false){
+        ShoppingCart.push({
+            Id: Id,
+            Qnt: 0,
+        });
+        localStorage.setItem("Shop", JSON.stringify(ShoppingCart));
+        ProductAction("Add",Id);
+    }else{
+        localStorage.setItem("Shop", JSON.stringify(ShoppingCart));
+        UpdateCart();
+    }
+}
